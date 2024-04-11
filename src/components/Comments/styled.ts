185beffbeaf1d046d73a6f3b82091cb71902f134
Felipe Batista <feliperodriguesbatista@outlook.com>
2024-04-11 09:23:40 -0300

    Fixed comments margin

--- src/components/Comments/styled.ts
+++ src/components/Comments/styled.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  margin: 5rem 0;
+  margin: 5rem 25rem 5rem 25rem;
 `;
--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -2,4 +2,4 @@ export const API_URL = 'https://blog-strapi-1.onrender.com/api';
 export const POSTS_URL = `${API_URL}/posts?populate=*`;
 
 export const SITE_NAME = 'Felipe Batista';
-export const SITE_URL = 'https://musical-sunburst-3890a4.netlify.app';
+export const SITE_URL = 'https://blog-felipe.netlify.app';
